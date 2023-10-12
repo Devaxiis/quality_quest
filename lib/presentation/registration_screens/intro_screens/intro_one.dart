828a2d86d07b443042d8740869c653252215fa60
macbookpro <coderikhbolsheikh@gmail.com>
2023-10-12 19:49:03 +0500

    Color Constants added

--- lib/presentation/registration_screens/intro_screens/intro_one.dart
+++ lib/presentation/registration_screens/intro_screens/intro_one.dart
@@ -9,6 +9,7 @@ class IntroOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColors.oxFFFFFFFF,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Center(
--- lib/presentation/registration_screens/intro_screens/intro_screen.dart
+++ lib/presentation/registration_screens/intro_screens/intro_screen.dart
@@ -17,6 +17,7 @@ class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColors.oxFFFFFFFF,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
