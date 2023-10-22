ba7933aa65e4e8cb9d24b32d726daa82684270b0
macbookpro <coderikhbolsheikh@gmail.com>
2023-10-22 19:42:31 +0500

    optimized

--- lib/data/network_service.dart
+++ lib/data/network_service.dart
@@ -1,24 +1,20 @@
+import 'dart:convert';
 
-import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
-import 'package:quality_quest/core/params/apis.dart'
-import 'dart:convert';
-
 import 'package:quality_quest/data/dio_interseptor.dart';
 import 'package:quality_quest/data/store.dart';
-import 'package:quality_quest/domain/model/registration/logo_out/logo_out.dart';
 
-
+import '../core/params/apis.dart';
+import '../domain/model/screens/category_model/category_model.dart';
 
 // abstract class Network {
-  // Future<void> methodPost({required String api,required Map<String, Object?> data});
-  // static Future<void> _saveToken(Map<String,dynamic> data);
-  // static Future<bool> methodSignUpPost({required String api,required Map<String, Object?> data});
-  // static Future<bool> methodSignInPost({required String api, required Map<String, Object?> data});
+// Future<void> methodPost({required String api,required Map<String, Object?> data});
+// static Future<void> _saveToken(Map<String,dynamic> data);
+// static Future<bool> methodSignUpPost({required String api,required Map<String, Object?> data});
+// static Future<bool> methodSignInPost({required String api, required Map<String, Object?> data});
 // }
-
 
 final dio = Dio();
 
@@ -30,15 +26,13 @@ class HttpService {
     _dio.interceptors.add(DioInterceptor());
   }
 
-
   // #Save Token
-  static Future<void> _saveToken(Map<String,dynamic> data)async{
-
+  static Future<void> _saveToken(Map<String, dynamic> data) async {
     final token = data["accessToken"];
     final refToken = data["refreshToken"];
     print("AccessToken:-----------------$token----------------------");
     print("RefreshToken:-----------------$refToken----------------------");
-    await Store.setToken(token,refToken);
+    await Store.setToken(token, refToken);
   }
 
   // #Method SignUp
@@ -86,7 +80,8 @@ class HttpService {
 
   // #Method GET Science Type
   static Future<List<ScienceType>> fetchScienceTypes() async {
-    final response = await dio.get("${Api.baseUrl}/api/Science/GetScienceTypes");
+    final response =
+        await dio.get("${Api.baseUrl}/api/Science/GetScienceTypes");
     if (response.statusCode == 200) {
       final List<dynamic> jsonList = response.data;
       return jsonList.map((json) => ScienceType.fromJson(json)).toList();
@@ -94,25 +89,29 @@ class HttpService {
       throw Exception('Failed to fetch science types');
     }
   }
-  
-   // #Method Logo0ut
-  static Future<bool> logoOut()async{
-    try{
+
+  // #Method Logo0ut
+  static Future<bool> logoOut() async {
+    try {
       await Store.clear();
       return true;
-    }catch(e){
+    } catch (e) {
       print("Error:--------$e---------");
     }
     return false;
   }
 
-
-  static Future<String?> multipart({String api = Api.addSciencePhoto, required String filePath, Map<String, String> headers = Api.headers, String baseUrl = Api.baseUrl, Map<String, String>? body}) async {
+  static Future<String?> multipart(
+      {String api = Api.addSciencePhoto,
+      required String filePath,
+      Map<String, String> headers = Api.headers,
+      String baseUrl = Api.baseUrl,
+      Map<String, String>? body}) async {
     final Uri url = Uri.https(baseUrl, api);
     final request = http.MultipartRequest("POST", url);
     request.headers.addAll(headers);
-    request.files.add(await http.MultipartFile.fromPath(
-        "file", filePath, contentType: MediaType("image", "jpeg")));
+    request.files.add(await http.MultipartFile.fromPath("file", filePath,
+        contentType: MediaType("image", "jpeg")));
     if (body != null) request.fields.addAll(body);
     http.StreamedResponse response = await request.send();
     if (response.statusCode == 200 || response.statusCode == 201) {
@@ -120,8 +119,5 @@ class HttpService {
     } else {
       return null;
     }
-
   }
-
-}
 }
