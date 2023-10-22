af45386f06955f8751f72727c397a2f36dce4412
Mirjalol1425 <mirjalolabdunazarov1425@gmail.com>
2023-10-22 16:00:07 +0500

    d

--- lib/presentation/screens/main_home_screen.dart
+++ lib/presentation/screens/main_home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quality_quest/domain/model/registration/token_model/token_mode.dart';
 import 'package:quality_quest/library.dart';
 import 'package:quality_quest/main.dart';
@@ -13,19 +14,43 @@ class MainHomeScreen extends StatefulWidget {
 class _MainHomeScreenState extends State<MainHomeScreen> {
   PageController pageController = PageController();
   int bottomNavbarIndex = 0;
+  int counter = 0;
   List<TokenModel> list =[];
 
   @override
   void initState() {
     super.initState();
   }
-    bool shouldPop = false;
+    // bool shouldPop = false;
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop:() async {
-        return shouldPop;
+      onWillPop: () async {
+        final shouldPop = await showDialog<bool>(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Do you want to go back?'),
+              actionsAlignment: MainAxisAlignment.spaceBetween,
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                  child:  Text('Yes',style: TextStyle(color: const Color(0xff6949ff),fontSize: 20.sp,fontWeight: FontWeight.bold),),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                  child:  Text('No',style: TextStyle(color: const Color(0xff6949ff),fontSize: 20.sp,fontWeight: FontWeight.bold),),
+                ),
+              ],
+            );
+          },
+        );
+        return shouldPop!;
       },
       child: Scaffold(
         /// #Backgroun color
