d4f998a058fa7073234cd87b074bc16a7db5d97d
macbookpro <coderikhbolsheikh@gmail.com>
2023-11-02 19:56:30 +0500

    Search Screen

--- lib/presentation/bloc/mein_home/search/search_bloc.dart
+++ lib/presentation/bloc/mein_home/search/search_bloc.dart
@@ -1,12 +1,12 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:quality_quest/core/params/apis.dart';
 import 'package:quality_quest/core/service_locator.dart';
+import 'package:quality_quest/domain/model/screens/search_model/search_screen_model.dart';
 import 'package:quality_quest/domain/repository/repository.dart';
 
 part 'search_event.dart';
+
 part 'search_state.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
@@ -14,18 +14,14 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
     on<SearchScienceEvent>(_searchScience);
   }
 
-  void _searchScience(SearchScienceEvent event, Emitter emit)async{
-
+  void _searchScience(SearchScienceEvent event, Emitter emit) async {
     emit(SearchLoading());
 
-
-
     final response = await (repository as RepositoryImplementation).network.methodSearchScience(api: Api.getSearchScience, data: event.title);
-    if(response){
-      emit(SearchSuccess());
-    }else{
+    if (response.isNotEmpty) {
+      emit(SearchSuccess(data: response));
+    } else {
       emit(SearchFailure());
     }
-
   }
 }
